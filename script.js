bad990215aed60f78835a4e05aafd1d45421bde3
mwalterFSDI <77034833+mwalterFSDI@users.noreply.github.com>
2021-01-30 19:42:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+alert("Thanks for Visiting!!");
+var name;
+var quantity;
+var price;
+
+name=prompt("enter product name");
+console.log(name);
+quantity=prompt("quantity");
+console.log(quantity);
+price=prompt("price");
+console.log(price);
+
+function product(num1,num2){
+    console.log(num1 * num2);
+}
+
